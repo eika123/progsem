4e3005aaa97249664413a0515e48208cc9cf2206
Eindride Kjersheim <eindride.kjersheim@gmail.com>
2018-10-18 08:15:29 +0200

    ..

--- aritmetisk_rekke.py
+++ aritmetisk_rekke.py
@@ -1,14 +1,12 @@
-print("dette er en test")
-
-
-a0 = 1  # try changing this - first element in progression
+a = 1  # try changing this - first element in progression
 d = 1   # difference between terms in sum
 
-s = a0
+s = 0
 N = 100
 
-for i in range(N + 1):
-    s = s + d
+for i in range(N):
+    s = s + a
+    a = a + d
 
 print(s)
 
